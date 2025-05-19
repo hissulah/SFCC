6a89b0b70fb9e1321916c7cf20b61468bd16a164
hissulah <housni.issulahabdoulhamid@chanel.com>
2025-05-19 17:52:07 +0200

    Update generate-pdf.js

--- tests/generate-pdf.js
+++ tests/generate-pdf.js
@@ -2,7 +2,9 @@ const puppeteer = require('puppeteer');
 const path = require('path');
 
 (async () => {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({
+        args: ['--no-sandbox', '--disable-setuid-sandbox']
+      });
   const page = await browser.newPage();
 
   const reportPath = `file://${path.resolve('report/index.html')}`;
